fbae0a50bc3a6f79f4d484266b5d66a116544577
Maarten Hoogendoorn <maarten@moretea.nl>
2016-10-13 14:47:51 +0200

    wip

---
+++ cmd/docker-machine-driver-qemu/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/moretea/docker-machine-qemu"
+	"github.com/docker/machine/libmachine/drivers/plugin"
+)
+
+func main() {
+	plugin.RegisterDriver(qemu.NewDriver("default", "path"))
+}
